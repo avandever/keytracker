507f513b6497db8d7b1d6b2a377f5a6e2039575c
Andrew Vandever <andrew.vandever@gmail.com>
2023-01-20 17:13:26 -0800

    Rollback db automatically on pending rollback error

--- keytracker/server.py
+++ keytracker/server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, jsonify
 from keytracker.schema import (
     db,
     Log,
@@ -12,6 +12,7 @@ from keytracker.routes import (
     ui,
     api,
 )
+from sqlalchemy.exc import PendingRollbackError
 import os
 
 
@@ -24,6 +25,12 @@ db.create_all()
 app.jinja_env.globals.update(render_log=render_log)
 app.register_blueprint(ui.blueprint)
 app.register_blueprint(api.blueprint)
+
+
+@app.errorhandler(PendingRollbackError)
+def handle_pending_rollback(error):
+    db.session.rollback()
+    return jsonify({"status_code": 500, "status": "Internal Server Error"})
 
 
 if __name__ == "__main__":
